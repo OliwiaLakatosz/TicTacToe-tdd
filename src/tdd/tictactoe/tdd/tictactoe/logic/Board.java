64ec34c3bfaa129a93960eded298d4216fc83f8a
Oliwia Lakatosz <olakatosz18@gmail.com>
2019-02-17 20:35:20 +0100

    win in column - implementation

--- src/tdd/tictactoe/tdd/tictactoe/logic/Board.java
+++ src/tdd/tictactoe/tdd/tictactoe/logic/Board.java
@@ -18,6 +18,7 @@ public class Board {
     }
 
     public boolean isWin() {
+        // Rows
         if (board.get(1).equals(board.get(2)) && board.get(2).equals(board.get(3))) {
             if (!board.get(1).equals("") && !board.get(2).equals("") && !board.get(3).equals("")) {
                 return true;
@@ -31,8 +32,24 @@ public class Board {
         if (board.get(7).equals(board.get(7)) && board.get(7).equals(board.get(7))) {
             if (!board.get(7).equals("") && !board.get(8).equals("") && !board.get(9).equals("")) {
                 return true;
-            }            }
+            }
+        }
+        // Columns
+        if (board.get(1).equals(board.get(4)) && board.get(4).equals(board.get(7))) {
+            if (!board.get(1).equals("") && !board.get(4).equals("") && !board.get(7).equals("")) {
+                return true;
+            }
+        }
+        if (board.get(2).equals(board.get(5)) && board.get(5).equals(board.get(8))) {
+            if (!board.get(2).equals("") && !board.get(5).equals("") && !board.get(8).equals("")) {
+                return true;
+            }
+        }
+        if (board.get(3).equals(board.get(6)) && board.get(6).equals(board.get(9))) {
+            if (!board.get(3).equals("") && !board.get(6).equals("") && !board.get(9).equals("")) {
+                return true;
+            }
+        }
         return false;
     }
-
 }
