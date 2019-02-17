abe4eb3a46f78c7a0b1bd267ad3227a2c5151dce
Oliwia Lakatosz <olakatosz18@gmail.com>
2019-02-17 20:30:54 +0100

    Tests for win in column - no implementation

--- src/tdd/tictactoe/tdd/tictactoe/logic/Board.java
+++ src/tdd/tictactoe/tdd/tictactoe/logic/Board.java
@@ -19,17 +19,17 @@ public class Board {
 
     public boolean isWin() {
         if (board.get(1).equals(board.get(2)) && board.get(2).equals(board.get(3))) {
-            if (!board.get(1).equals("")) {
+            if (!board.get(1).equals("") && !board.get(2).equals("") && !board.get(3).equals("")) {
                 return true;
             }
         }
         if (board.get(4).equals(board.get(5)) && board.get(5).equals(board.get(6))) {
-            if (!board.get(4).equals("")) {
+            if (!board.get(4).equals("") && !board.get(5).equals("") && !board.get(6).equals("")) {
                 return true;
             }
         }
         if (board.get(7).equals(board.get(7)) && board.get(7).equals(board.get(7))) {
-            if (!board.get(7).equals("")) {
+            if (!board.get(7).equals("") && !board.get(8).equals("") && !board.get(9).equals("")) {
                 return true;
             }            }
         return false;
--- test/tdd/tictactoe/test/BoardTest.java
+++ test/tdd/tictactoe/test/BoardTest.java
@@ -67,4 +67,34 @@ public class BoardTest {
         System.out.println(testBoard);
         Assert.assertFalse(board.isWin());
     }
+
+    @Test
+    public void testCheckWinInFirstColumn() {
+        Map<Integer, String> testBoard = board.getBoard();
+        testBoard.put(1, "X");
+        testBoard.put(4, "X");
+        testBoard.put(7, "X");
+        System.out.println(testBoard);
+        Assert.assertTrue(board.isWin());
+    }
+
+    @Test
+    public void testCheckWinInSecondColumn() {
+        Map<Integer, String> testBoard = board.getBoard();
+        testBoard.put(2, "X");
+        testBoard.put(5, "X");
+        testBoard.put(8, "X");
+        System.out.println(testBoard);
+        Assert.assertTrue(board.isWin());
+    }
+
+    @Test
+    public void testCheckWinInThirdColumn() {
+        Map<Integer, String> testBoard = board.getBoard();
+        testBoard.put(3, "X");
+        testBoard.put(6, "X");
+        testBoard.put(9, "X");
+        System.out.println(testBoard);
+        Assert.assertTrue(board.isWin());
+    }
 }
