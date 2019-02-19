d4845a8ae811e32da4b8aa31e48c58453d52d7b0
Oliwia Lakatosz <olakatosz18@gmail.com>
2019-02-19 20:28:27 +0100

    Update FlowController.java

--- src/tdd/tictactoe/FlowController.java
+++ src/tdd/tictactoe/FlowController.java
@@ -11,6 +11,7 @@ class FlowController {
     private static Board boardObject = new Board();
     private static Font font = new Font("Arial", Font.BOLD, 80);
     private static String[] PLAYER = {"O", "X"};
+    private static Object[] options = {"Play Again", "Exit"};
     private static int playerIndex = 0;
 
     static void clickButton(Button button) {
@@ -29,7 +30,21 @@ class FlowController {
     static void announceWinner(String winner) {
         if (boardObject.isWin()) {
             String msg = "Winner: " + winner;
-            JOptionPane.showMessageDialog(BoardView.getMainWindow(), msg);
+            JOptionPane optionPane = new JOptionPane();
+            int option = optionPane.showOptionDialog(BoardView.getMainWindow(), msg, "Game Over",
+                    JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE,
+                    null, options, options[0]);
+            if (option == 0) {
+                playAgain();
+            } else if (option == 1) {
+                BoardView.getMainWindow().dispose();
+            }
+
         }
+    }
+
+    static void playAgain() {
+        // TODO: implement reinit method - clear board map and gui
+        System.out.println("To be implemented!");
     }
 }
